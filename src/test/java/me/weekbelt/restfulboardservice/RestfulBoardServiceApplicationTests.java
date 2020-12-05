4e414a02b9afb9ac5cfc97cf763db353d34e2f87
weekbelt <vfrvfr4207@gmail.com>
2020-12-05 19:18:23 +0900

    Initial commit

---
+++ src/test/java/me/weekbelt/restfulboardservice/RestfulBoardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.weekbelt.restfulboardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulBoardServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
